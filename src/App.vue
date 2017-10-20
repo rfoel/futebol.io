7b042c7762ef91803205473e1175379abcc826a8
Rafael Franco <rafaelfr@outlook.com>
2017-10-20 19:32:39 -0200

    Added vue-jsonp

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <img src="./assets/futebol.io.svg">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  created() {
+    this.$jsonp(
+      "https://jsuol.com.br/c/monaco/utils/gestor/commons.js?callback=simulador_dados_jsonp&file=commons.uol.com.br%2Fsistemas%2Fesporte%2Fmodalidades%2Ffutebol%2Fcampeonatos%2Fdados%2F2017%2F30%2Fdados.json"
+    )
+      .then(json => {
+        console.log(json)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue"
+import App from "./App"
+import VueJsonp from "vue-jsonp"
 
+Vue.use(VueJsonp)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
+  el: "#app",
+  template: "<App/>",
   components: { App }
 })
