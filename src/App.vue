ecd9236a68f2e3e896e3bfea3c34f080fd2aebba
Rafael Franco <rafaelfr@outlook.com>
2017-10-20 23:00:41 -0200

    Added props

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,68 @@
 <template>
   <div id="app">
     <header>
-      <img src="./assets/futebol.io.svg" class="logo"><h1>futebol.io</h1>
+      <img src="./assets/futebol.io.svg" class="logo">
+      <h1>futebol.io</h1>
     </header>
     <div class="error" v-if="error">
       {{error}}
     </div>
     <div v-if="data">
       <h2>{{data["nome-completo"]}}</h2>
+      <StandingsTable :data="data" />
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "app",
-  data() {
-    return {
-      data: null,
-      error: null
+  import StandingsTable from "./components/StandingsTable"
+  
+  export default {
+    name: "app",
+    components: {
+      StandingsTable
+    },
+    data() {
+      return {
+        data: null,
+        error: null
+      }
+    },
+    created() {
+      this.$jsonp(
+          "https://jsuol.com.br/c/monaco/utils/gestor/commons.js?file=commons.uol.com.br/sistemas/esporte/modalidades/futebol/campeonatos/dados/2017/30/dados.json"
+        )
+        .then(json => {
+          this.data = json
+          console.log(json)
+        })
+        .catch(error => {
+          this.error = error
+        })
     }
-  },
-  created() {
-    this.$jsonp(
-      "https://jsuol.com.br/c/monaco/utils/gestor/commons.js?file=commons.uol.com.br/sistemas/esporte/modalidades/futebol/campeonatos/dados/2017/30/dados.json"
-    )
-      .then(json => {
-        this.data = json
-      })
-      .catch(error => {
-        this.error = error
-      })
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: "Dosis", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-header {
-}
-
-header h1 {
-  display: inline-block;
-  padding: 20px;
-  line-height: 100px;
-}
-.logo {
-  display: inline-block;
-  width: 50px;
-}
+  #app {
+    font-family: "Dosis", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  header {}
+  
+  header h1 {
+    display: inline-block;
+    padding: 20px;
+    line-height: 100px;
+  }
+  
+  .logo {
+    display: inline-block;
+    width: 50px;
+  }
 </style>
---
+++ src/components/StandingsTable.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <th>#</th>
+        <th>Team</th>
+        <th>MP</th>
+        <th>W</th>
+        <th>D</th>
+        <th>L</th>
+        <th>GF</th>
+        <th>GA</th>
+        <th>GD</th>
+        <th>Pts</th>
+      </thead>
+      <tbody>
+        <tr v-for='(team, index) in data.equipes' :key='index'>
+          <td>{{team}}</td>
+          <td>Team</td>
+          <td>MP</td>
+          <td>W</td>
+          <td>D</td>
+          <td>L</td>
+          <td>GF</td>
+          <td>GA</td>
+          <td>GD</td>
+          <td>Pts</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "StandingsTable",
+    props: ["data"],
+  }
+</script>
+
+<style lang="scss" scoped>
+  table {
+    width: 100%;
+    tbody {
+      padding: 50px 0;
+      td {
+        padding: 10px;
+      }
+    }
+  }
+</style>
