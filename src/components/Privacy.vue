fef7703d4be030644af5ebbdb67691ec5a2ecca4
Rafael Franco <rafaelfr@outlook.com>
2018-01-14 14:44:12 -0200

    Updated privacy

--- src/components/Privacy.vue
+++ src/components/Privacy.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="container">
-    <div class="section">
-      <p>
-        NÓS NÃO PODEMOS VER OU GUARDAR QUALQUER DADO SEU. PONTO.
-      </p>
-      <p>
-        Nós fisicamente não podemos. Não temos onde armazená-lo. Nós nem sequer temos um banco de dados de servidor para armazená-lo. Então, mesmo que Justin Bieber pedisse para ver seus dados, não teríamos nada para mostrá-lo.
-      </p>
-      <p>
-        É por isso que, com o <strong>Futebol.io</strong>, o que acontece no seu computador permanece no seu computador.  
-      </p>
+    <div class="columns">
+      <div class="column"></div>
+      <div class="section column is-three-quarters">
+        <h1 class="title">
+          Política de Privacidade
+        </h1>
+        <p>
+          NÓS NÃO PODEMOS VER OU GUARDAR QUALQUER DADO SEU. PONTO.
+        </p>
+        <br>
+        <p>
+          Nós fisicamente não podemos. Não temos onde armazená-lo. Nós nem sequer temos um banco de dados para armazená-lo. Então, mesmo que Justin Bieber pedisse para ver seus dados, não teríamos nada para mostrá-lo.
+        </p>
+        <br>
+        <p>
+          É por isso que, com o <strong>Futebol.io</strong>, o que acontece no seu computador permanece no seu computador.  
+        </p>
+      </div>
+      <div class="column"></div>    
     </div>
   </div>
 </template>
@@ -19,5 +28,4 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ export default new Router({
       component: Home
     },
     {
-      path: '/:league',
+      path: '/@:league',
       name: 'league',
       component: League
     },
