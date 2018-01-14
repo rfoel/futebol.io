782eca118aec8e0d3d4a5b4dfb62202d0be9064e
Rafael Franco <rafaelfr@outlook.com>
2018-01-14 14:35:11 -0200

    Added privacy

---
+++ src/components/Privacy.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="section">
+      <p>
+        NÓS NÃO PODEMOS VER OU GUARDAR QUALQUER DADO SEU. PONTO.
+      </p>
+      <p>
+        Nós fisicamente não podemos. Não temos onde armazená-lo. Nós nem sequer temos um banco de dados de servidor para armazená-lo. Então, mesmo que Justin Bieber pedisse para ver seus dados, não teríamos nada para mostrá-lo.
+      </p>
+      <p>
+        É por isso que, com o <strong>Futebol.io</strong>, o que acontece no seu computador permanece no seu computador.  
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import League from '@/components/League'
+import Privacy from '@/components/Privacy'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/:league',
       name: 'league',
       component: League
+    },
+    {
+      path: '/politica-de-privacidade',
+      name: 'privacy',
+      component: Privacy
     }
   ]
 })
