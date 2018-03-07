9cc1c279e3754548168e47901f04a0c6beba4a05
Rafael Franco <rafaelfr@outlook.com>
2018-03-06 23:29:22 -0300

    Added compression

--- web/server.js
+++ web/server.js
@@ -3,11 +3,13 @@ const history = require('connect-history-api-fallback')
 const path = require('path')
 const request = require('request')
 const serveStatic = require('serve-static')
+const compression = require('compression')
 
 const app = express()
 
+app.use(compression())
 app.use(history({ verbose: true }))
-app.use(serveStatic(path.join(__dirname, '/dist')))
+app.use(serveStatic(path.join(__dirname, '/dist')), { maxAge: '1d' })
 
 app.listen(8080, function() {
   console.log(
