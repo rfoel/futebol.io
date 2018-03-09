7e2e172efe01a004c52c2037ee9bfe14961b925f
Rafael Franco <rafaelfr@outlook.com>
2018-03-09 09:32:59 -0300

    Updated ssri for web

--- web/src/components/layout/PageFooter.vue
+++ web/src/components/layout/PageFooter.vue
@@ -4,17 +4,20 @@
       <div class="content has-text-centered">
         <div class="share">
           <div class="fb-share-button" data-href="http://futebol.io" data-layout="button" data-size="large" data-mobile-iframe="true">
-            <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Ffutebol.io%2F&amp;src=sdkpreparse">Compartilhar</a>
+            <a class="fb-xfbml-parse-ignore" target="_blank" rel="noopener" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Ffutebol.io%2F&amp;src=sdkpreparse">Compartilhar</a>
           </div>
           <a href="https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-size="large" data-text="⚽️"
             data-url="http://futebol.io/" data-via="rfoel" data-related="rfoel" data-hashtags="SemFrescuraSoTabelas" data-dnt="true"
             data-show-count="false">Tweet</a>
         </div>
+        <p>
+          <small class="has-text-grey-light">futebol.io API em breve</small>
+        </p>
         <small>
           <i class="fas fa-code"></i> com
           <i class="fas fa-heart has-text-danger"></i>
           por
-          <a href="http://rafaelfran.co" target="_blank">Rafael Franco</a>
+          <a href="http://rafaelfran.co" target="_blank" rel="noopener">Rafael Franco</a>
         </small>
       </div>
     </div>
@@ -26,7 +29,7 @@ footer {
   padding: 1.5rem 0;
   .share {
     padding-bottom: 1rem;
-    line-height: .74;
+    line-height: 0.74;
   }
 }
 </style>
